54bd3210c4d2e4c365f925f4bd7644f1a12af0fa
Xsidz <siddhesh.kabraa@gmail.com>
2025-08-08 23:11:02 +0530

    Startupcard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -7,6 +8,19 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1 , name : "Author !2"},
+      _id: 1,
+      descrption: "this is an descrpiton",
+      image:
+        "https://www.pexels.com/photo/green-and-blue-peacock-feather-674010/",
+      category: "feather",
+      title: "Feather Pic",
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -18,6 +32,21 @@ export default async function Home({
           Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ?(
+            posts.map((post :StartupCardType,index: number)=>(
+              <StartupCard key={post?._id} post={post}/>
+            )
+          )):(
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
